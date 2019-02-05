5ddbab5b38b65be4dc1cc6037a680c6f23701b93
Peter Broadwell <broadwell@stanford.edu>
2019-02-05 00:13:28 -0800

    Updates

---
+++ assets/main.f051b1ac6017c403ca83.hot-update.js
@@ -0,0 +1,21 @@
+webpackHotUpdate("main",{
+
+/***/ "./js/components/Image.js":
+false,
+
+/***/ "./js/components/MiradorContainer.js":
+/*!*******************************************!*\
+  !*** ./js/components/MiradorContainer.js ***!
+  \*******************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _MiradorViewer__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./MiradorViewer */ \"./js/components/MiradorViewer.jsx\");\n //import Image from \"./Image\";\n//import miradorLarge from \"./images/mirador-3-1.png\";\n\n\n\nvar MiradorContainer = function MiradorContainer() {\n  //return <Image src={miradorLarge} width={1175} height={797} />;\n  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_MiradorViewer__WEBPACK_IMPORTED_MODULE_1__[\"default\"], null);\n};\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (MiradorContainer);\n\n//# sourceURL=webpack:///./js/components/MiradorContainer.js?");
+
+/***/ }),
+
+/***/ "./js/components/images/mirador-3-1.png":
+false
+
+})

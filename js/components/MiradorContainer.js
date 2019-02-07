8f8ff8185f34c69b5d2d68dc2ecb530a6f39a4ff
Peter Broadwell <broadwell@stanford.edu>
2019-02-06 19:13:00 -0800

    Band-aids on mirador viewer functionality to make it work

--- js/components/MiradorContainer.js
+++ js/components/MiradorContainer.js
@@ -1,13 +1,10 @@
 import React from "react";
 
-import Image from "./Image";
-import miradorLarge from "./images/mirador-3-1.png";
-
-//import MiradorViewer from './MiradorViewer';
+import MiradorViewer from './MiradorViewer';
 
 const MiradorContainer = () => {
-  return <Image src={miradorLarge} width={1175} height={797} />;
-  //return <MiradorViewer />;
+  //return <Image src={miradorLarge} width={1175} height={797} />;
+  return <MiradorViewer />;
 };
 
 export default MiradorContainer;
--- js/index.jsx
+++ js/index.jsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 //import "./components/index.css";
 import App from "./components/App";
 
-//import MiradorViewer from './components/MiradorViewer';
-
 ReactDOM.render(<App />, document.getElementById("app"));
 
 module.hot.accept();

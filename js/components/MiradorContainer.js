5e3c5b417f5dfc3b9a7f8999cf5a4bc76d210bec
Peter Broadwell <broadwell@stanford.edu>
2019-02-08 10:00:00 -0800

    Remove placeholder image

--- js/components/MiradorContainer.js
+++ js/components/MiradorContainer.js
@@ -3,7 +3,6 @@ import React from "react";
 import MiradorViewer from './MiradorViewer';
 
 const MiradorContainer = () => {
-  //return <Image src={miradorLarge} width={1175} height={797} />;
   return <MiradorViewer />;
 };
 

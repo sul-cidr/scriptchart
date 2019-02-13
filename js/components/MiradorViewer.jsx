33cb3ea8a999f00b20dc7208b516720578937db8
Peter Broadwell <broadwell@stanford.edu>
2019-02-13 00:44:44 -0800

    Add BnF manuscript to Mirador list, even though the images don't display properly

--- js/components/MiradorViewer.jsx
+++ js/components/MiradorViewer.jsx
@@ -18,9 +18,8 @@ export default class MiradorViewer extends Component {
         { manifestUri: "https://digi.vatlib.it/iiif/MSS_Vat.sir.157/manifest.json", "location": "Biblioteca Apostolica Vaticana"},
         { manifestUri: "https://digi.vatlib.it/iiif/MSS_Vat.sir.161/manifest.json", "location": "Biblioteca Apostolica Vaticana"},
         { manifestUri: "https://purl.stanford.edu/zv668dm4974/iiif/manifest", "location": "Stanford University"},
-        { manifestUri: "https://iiif.harvardartmuseums.org/manifests/object/320567", "location": "Harvard University"},
-        { manifestUri: "https://media.nga.gov/public/manifests/nga_highlights.json", "location": "National Gallery of Art"},
-        { manifestUri: "https://media.nga.gov/public/manifests/multispectral_demo.json", "location": "National Gallery of Art"},
+        { manifestUri: "https://gallica.bnf.fr/iiif/ark:/12148/btv1b531151912/manifest.json", "location": "Bibliotheque Nationale"},
+        { manifestUri: "https://gallica.bnf.fr/iiif/ark:/12148/btv1b10528606c/manifest.json", "location": "Bibliotheque Nationale"}
       ],
       windowObjects: [{
         loadedManifest: "https://digi.vatlib.it/iiif/MSS_Vat.sir.161/manifest.json",

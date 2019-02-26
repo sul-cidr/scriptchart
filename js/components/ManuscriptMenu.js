1ecc4ecde379451c2973eb97e06470c3794913df
Peter Broadwell <broadwell@stanford.edu>
2019-02-25 18:31:28 -0800

    Remove useless constructor

--- js/components/ManuscriptMenu.js
+++ js/components/ManuscriptMenu.js
@@ -3,10 +3,6 @@ import React from "react";
 import ManuscriptsLoader from "./ManuscriptsLoader";
 
 class ManuscriptMenu extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className={"field"}>

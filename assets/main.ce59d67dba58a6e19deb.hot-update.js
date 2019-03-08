2091f04f2bc6da119f6dd62c2cbd920f39e41438
Peter Broadwell <broadwell@stanford.edu>
2019-03-07 18:14:41 -0800

    Updates

---
+++ assets/main.ce59d67dba58a6e19deb.hot-update.js
@@ -0,0 +1,17 @@
+webpackHotUpdate("main",{
+
+/***/ "./js/components/ManuscriptHeader.js":
+/*!*******************************************!*\
+  !*** ./js/components/ManuscriptHeader.js ***!
+  \*******************************************/
+/*! exports provided: default */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: ENOENT: no such file or directory, open '/Users/pmb/Documents/dash/scriptchart/js/components/ManuscriptHeader.js'\");\n\n//# sourceURL=webpack:///./js/components/ManuscriptHeader.js?");
+
+/***/ }),
+
+/***/ "./node_modules/@fortawesome/react-fontawesome/index.es.js":
+false
+
+})

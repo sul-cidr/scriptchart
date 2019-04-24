187b26cd4bf51bb97d4535799c75fdd43d70a8ec
Peter Broadwell <broadwell@stanford.edu>
2019-04-24 14:23:35 -0700

    Remove FontAwesomeIcon components to avoid test warning

--- js/components/ColumnControls.js
+++ js/components/ColumnControls.js
@@ -7,7 +7,7 @@ import React from "react";
  * it to the ChartAccordion list.
  */
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class ColumnControls extends React.Component {
   constructor(props) {
@@ -28,16 +28,19 @@ class ColumnControls extends React.Component {
   }
 
   render() {
+   /* Using the FontAwesomeIcon component  causes a warning in the test
+    * output for the fa-info-circle. No idea why, but for consistency we
+    * might as well use the basic <i> element for all of them until we
+    * know what the problem is.
+    */
     let manifestSpan = "";
     if (this.props.manifestURL != null) {
       manifestSpan = (
         <div className={"header-icon"}>
-          <FontAwesomeIcon
+          <i class="fa fa-book-open"
             title="Show this manuscript in the manuscripts tab"
-            color="#0000FF"
-            icon="book-open"
+            style={{ cursor: "pointer", color: "#0000FF" }}
             onClick={this.showManifest}
-            style={{ cursor: "pointer" }}
           />
         </div>
       );
@@ -54,11 +57,9 @@ class ColumnControls extends React.Component {
           <b>&#10007;</b>
         </div>
         <div className={"header-icon"}>
-          <FontAwesomeIcon
+          <i class="fa fa-info-circle"
             title="Information about this manuscript"
-            color="#0000FF"
-            icon="info-circle"
-            style={{ cursor: "pointer" }}
+            style={{ cursor: "pointer", color: "#0000FF" }}
           />
         </div>
         {manifestSpan}
